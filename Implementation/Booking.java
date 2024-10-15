a0c9c47e1623edbaf560b5931a630eae7d6d1350
Sardar Noman Shakir <121171686+CodeWithZavi@users.noreply.github.com>
2024-10-15 08:24:00 -0700

    Update Booking.java

--- Implementation/Booking.java
+++ Implementation/Booking.java
@@ -3,10 +3,11 @@ public class Booking {
     private int seatNumber;
     private double price;
 
+    // Creator Principle: Booking is instantiated with bus and seat details
     public Booking(Bus bus, int seatNumber) {
         this.bus = bus;
         this.seatNumber = seatNumber;
-        this.price = bus.getPrice();
+        this.price = bus.getPrice(); // Get the price from the bus information
     }
 
     public String getReceipt() {
@@ -16,4 +17,6 @@ public class Booking {
                 "Seat: " + seatNumber + "\n" +
                 "Price: $" + price;
     }
+
+    // Additional methods related to booking can be added here
 }
